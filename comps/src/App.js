4739af5d14ce1253f561540625462fb4f345ac02
HuiGong-dev <hellohuigong@gmail.com>
2023-04-15 15:47:11 +0200

    add button props

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button primary>click me</Button>
+      </div>
+      <div>
+        <Button>buy now</Button>
+      </div>
+      <div>
+        <Button>see deal</Button>
+      </div>
+      <div>
+        <Button>hide ads</Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
     </div>
   );
 }
