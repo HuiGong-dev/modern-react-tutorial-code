70a590a379edd8512db4b4c0279a7a989311d8b4
HuiGong-dev <hellohuigong@gmail.com>
2023-04-24 13:26:23 +0200

    detect click outside of dropdown

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { GoChevronDown, GoChevronLeft } from 'react-icons/go';
+import Panel from './Panel';
 
 const Dropdown = ({ options, value, onChange }) => {
   const [isHidden, setIsHidden] = useState(true);
+  const divEl = useRef();
+  useEffect(() => {
+    const handler = (event) => {
+      if (!divEl.current) {
+        return;
+      }
+      if (!divEl.current.contains(event.target)) {
+        setIsHidden(true);
+      }
+    };
+    document.addEventListener('click', handler, true);
+    const cleanUp = () => {
+      document.removeEventListener('click', handler);
+    };
+    return cleanUp;
+  }, []);
   const icon = (
     <span className="text-xl">
       {isHidden ? <GoChevronLeft /> : <GoChevronDown />}
@@ -17,6 +34,7 @@ const Dropdown = ({ options, value, onChange }) => {
   };
   const renderedOptions = options.map((option) => (
     <div
+      className="hover:bg-sky-100 rounded cursor-pointer px-2 py-1"
       key={option.value}
       onClick={() => {
         handleOptionClick(option);
@@ -26,12 +44,17 @@ const Dropdown = ({ options, value, onChange }) => {
     </div>
   ));
   return (
-    <div>
-      <div className="flex" onClick={handleClick}>
+    <div ref={divEl} className="w-48 relative">
+      <Panel
+        className="flex justify-between items-center cursor-pointer "
+        onClick={handleClick}
+      >
         {value?.label || 'Select...'}
         {icon}
-      </div>
-      {!isHidden && renderedOptions}
+      </Panel>
+      {!isHidden && (
+        <Panel className="absolute top-full  p-2  ">{renderedOptions}</Panel>
+      )}
     </div>
   );
 };
